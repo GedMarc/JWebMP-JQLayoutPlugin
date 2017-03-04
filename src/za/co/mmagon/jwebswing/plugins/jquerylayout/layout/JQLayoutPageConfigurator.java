ebb26708a5bc817b57defc7049f90592200b4f8d
Marc Magon <ged_marc@hotmail.com>
2017-03-04 09:24:24 +0200

    v0.9

--- src/za/co/mmagon/jwebswing/plugins/jquerylayout/layout/JQLayoutPageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/jquerylayout/layout/JQLayoutPageConfigurator.java
@@ -1,10 +1,10 @@
-/*
- * Copyright (C) 2017 GedMarc
+/* 
+ * Copyright (C) 2017 Marc Magon
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package za.co.mmagon.jwebswing.plugins.jquerylayout.layout;
 
@@ -27,15 +26,22 @@ import za.co.mmagon.jwebswing.plugins.PluginInformation;
  * @since 27 Feb 2017
  *
  */
-@PluginInformation(pluginName = "JQuery UI Layout", pluginUniqueName = "jquery-layout",
-        pluginDescription = "The UI Layout plug-in can create any UI look you want - from simple headers or sidebars, to a complex application with toolbars, menus, help-panels, status bars, sub-forms, etc.",
-        pluginVersion = "1.5.13",
-        pluginDependancyUniqueIDs = "", pluginCategories = "jquery, layouts,border layout, jwebswing",
-        pluginSubtitle = "The Ultimate Page Layout Manager",
-        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-JQLayoutPlugin",
-        pluginSourceUrl = "https://github.com/GedMarc/layout",
-        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-JQLayoutPlugin/wiki",
-        pluginOriginalHomepage = "http://layout.jquery-dev.com/")
+@PluginInformation(pluginName = "JQuery UI Layout",
+                   pluginUniqueName = "jquery-layout",
+                   pluginDescription = "The UI Layout plug-in can create any UI look you want - from simple headers or sidebars, to a complex application with toolbars, menus, help-panels, status bars, sub-forms, etc.",
+                   pluginVersion = "1.5.13",
+                   pluginDependancyUniqueIDs = "jquery",
+                   pluginCategories = "jquery, layouts,border layout, jwebswing",
+                   pluginSubtitle = "The Ultimate Page Layout Manager",
+                   pluginGitUrl = "https://github.com/GedMarc/JWebSwing-JQLayoutPlugin",
+                   pluginSourceUrl = "https://github.com/GedMarc/layout",
+                   pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-JQLayoutPlugin/wiki",
+                   pluginOriginalHomepage = "http://layout.jquery-dev.com/",
+                   pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/JQLayoutPlugin.jar/download",
+                   pluginIconUrl = "bower_components/jquery-layout-latest-1-5/borderlayout.gif",
+                   pluginIconImageUrl = "bower_components/jquery-layout-latest-1-5/borderlayout.gif",
+                   pluginLastUpdatedDate = "2017/03/04"
+)
 public class JQLayoutPageConfigurator extends PageConfigurator
 {
 
