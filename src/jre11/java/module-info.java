54b11104e9953f0800cbaccc16ce3d6628210b4a
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:40 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -8,7 +8,7 @@ module com.jwebmp.plugins.jqlayout {
 	exports com.jwebmp.plugins.jqlayout.options;
 
 	requires transitive com.jwebmp.core;
-	requires transitive com.jwebmp.logmaster;
+	requires transitive com.guicedee.logmaster;
 	requires transitive com.jwebmp.plugins.jqueryui;
 
 	requires com.fasterxml.jackson.annotation;
@@ -17,12 +17,12 @@ module com.jwebmp.plugins.jqlayout {
 	requires java.logging;
 
 	requires com.jwebmp.plugins.easingeffects;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.jqlayout.JQLayoutPageConfigurator;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.jqlayout.implementations.JQLayoutExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.jqlayout.implementations.JQLayoutExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.jqlayout.implementations.JQLayoutExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.jqlayout.implementations.JQLayoutExclusionsModule;
 
 	opens com.jwebmp.plugins.jqlayout to com.fasterxml.jackson.databind, com.jwebmp.core;
 	opens com.jwebmp.plugins.jqlayout.interfaces to com.fasterxml.jackson.databind, com.jwebmp.core;
